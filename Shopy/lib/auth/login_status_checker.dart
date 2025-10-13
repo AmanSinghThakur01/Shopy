98e3db3cfb8cc8038aa8515bc2812ec293a5669a
AmanSinghThakur01 <thakuramansinghh01@gmail.com>
2025-10-13 23:05:50 +0530

    login Status checker file created

---
+++ Shopy/lib/auth/login_status_checker.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shopy/auth/loginpage.dart';
+import 'package:shopy/presentation/user/screens/Homepage.dart';
+
+class LoginStatusChecker extends StatefulWidget {
+  const LoginStatusChecker({super.key});
+
+  @override
+  State<LoginStatusChecker> createState() => _LoginStatusCheckerState();
+}
+
+class _LoginStatusCheckerState extends State<LoginStatusChecker> {
+  @override
+  Widget build(BuildContext context) {
+    return checkuser();
+
+  }
+  checkuser()async {
+    final user =  FirebaseAuth.instance.currentUser;
+    if (user!= null){
+      return Homepage();
+    }
+    else {
+      return Loginpage();
+    }
+  }
+}

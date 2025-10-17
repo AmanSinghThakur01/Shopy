c7ea57faa3bed40f19972edd69c496efae3e7ac9
AmanSinghThakur01 <thakuramansinghh01@gmail.com>
2025-10-18 00:00:53 +0530

    bottom navigation bar added

--- Shopy/lib/presentation/user/screens/FirstPage.dart
+++ Shopy/lib/presentation/user/screens/FirstPage.dart
@@ -5,6 +5,7 @@ import 'package:shopy/presentation/user/screens/HomeScreen.dart';
 import 'package:shopy/presentation/user/screens/SearchScreen.dart';
 import 'package:shopy/presentation/user/screens/Setting_Screen.dart';
 import 'package:shopy/presentation/user/screens/WishlistScreen.dart';
+
 class Firstpage extends StatefulWidget {
   const Firstpage({super.key});
 
@@ -15,46 +16,51 @@ class Firstpage extends StatefulWidget {
 class _FirstpageState extends State<Firstpage> {
   List _pages = [
     Homescreen(),
-   Wishlistscreen(),
+    Wishlistscreen(),
     Cartscreen(),
     Searchscreen(),
     SettingScreen(),
-
   ];
-  int _selectedIndex = 0 ;
-  void setIndex ( int index){
+
+  int _selectedIndex = 0;
+
+  void setIndex(int index) {
     setState(() {
-      _selectedIndex = index ;
+      _selectedIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      centerTitle: true,
-      title: Text(
-        "Shopy",
-        style: TextStyle(
-          color: Colors.blueAccent,
-          fontSize: 22,
-          fontWeight: FontWeight.bold,
+        centerTitle: true,
+        title: const Text(
+          "Shopy",
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        actions: const [Icon(Icons.person)],
       ),
-      actions: const [Icon(Icons.person)],
-    ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.tealAccent,
-          selectedItemColor: AppColors.secondary,
-          currentIndex: _selectedIndex,
-          onTap: setIndex,
-          items:[
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: ' Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: ' Wishlist'),
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ' Cart'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: ' Search'),
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: ' Settings'),
-      ]),
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: setIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: 'Wishlist'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
     );
   }
 }

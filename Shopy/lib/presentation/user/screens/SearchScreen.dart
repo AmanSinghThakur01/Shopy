929f2e2b9ba22e746961d79ef4bf5bb9a6456fca
AmanSinghThakur01 <thakuramansinghh01@gmail.com>
2025-11-16 04:07:34 +0530

    working on search screen

--- Shopy/lib/presentation/user/screens/SearchScreen.dart
+++ Shopy/lib/presentation/user/screens/SearchScreen.dart
@@ -8,11 +8,50 @@ class Searchscreen extends StatefulWidget {
 }
 
 class _SearchscreenState extends State<Searchscreen> {
+  final TextEditingController _searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppAppbar(),
-      body: Text("search Page"),
+      body: SafeArea(child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _searchController,
+              onChanged: performSearch,
+              onSubmitted: (value) {
+                addToRecentSearch(value);
+                performSearch(value);
+              },
+              decoration: InputDecoration(
+                hintText: "Search for products...",
+                prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                suffixIcon: _searchController.text.isNotEmpty
+                    ? IconButton(
+                  icon: const Icon(Icons.clear, color: Colors.grey),
+                  onPressed: () {
+                    _searchController.clear();
+                    performSearch('');
+                  },
+                )
+                    : null,
+                filled: true,
+                fillColor: Colors.grey.shade100,
+                contentPadding: const EdgeInsets.symmetric(vertical: 12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(color: Colors.pink.shade300, width: 2),
+                ),
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
